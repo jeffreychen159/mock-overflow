cfd007dd454398dbfb3a35c51308d36edbd5e32e
jeffreychen159 <chanjeffrey123@gmail.com>
2024-04-08 15:45:42 -0400

    refactored to fit with this structure, plan to implement more features

--- server/models/answers.js
+++ server/models/answers.js
@@ -3,4 +3,4 @@ const mongoose = require("mongoose");
 
 const Answer = require("./schema/answer");
 
-module.exports = mongoose.model("fake_so", Answer);
+module.exports = mongoose.model("Answer", Answer);
--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@
 //     process.exit(0);
 // });
 
+
 // Run this script to launch the server.
 // The server should run on localhost port 8000.
 // This is where you should start writing server-side code for this application.
