98e6421187effd381ee381544831c77b5ce7530c
jeffreychen159 <chanjeffrey123@gmail.com>
2024-04-08 14:54:16 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,67 @@
-// Application server
+// // Application server
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const { MONGO_URL, port } = require("./config");
+
+// mongoose.connect(MONGO_URL);
+
+// const app = express();
+
+// app.get("/", (_, res) => {
+//     res.send("Fake SO Server Dummy Endpoint");
+//     res.end();
+// });
+
+// let server = app.listen(port, () => {
+//     console.log(`Server starts at http://localhost:${port}`);
+// });
+
+// process.on("SIGINT", () => {
+//     server.close();
+//     mongoose.disconnect();
+//     console.log("Server closed. Database instance disconnected");
+//     process.exit(0);
+// });
+
+// Run this script to launch the server.
+// The server should run on localhost port 8000.
+// This is where you should start writing server-side code for this application.
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-const { MONGO_URL, port } = require("./config");
+const MONGO_URL = "mongodb://127.0.0.1:27017/fake_so";
+const CLIENT_URL = "http://localhost:3000";
+const port = 8000;
 
 mongoose.connect(MONGO_URL);
 
 const app = express();
 
-app.get("/", (_, res) => {
-    res.send("Fake SO Server Dummy Endpoint");
+app.use(
+    cors({
+        credentials: true,
+        origin: [CLIENT_URL],
+    })
+);
+
+app.use(express.json());
+
+app.get("", (req, res) => {
+    res.send("hello world");
     res.end();
 });
+
+const questionController = require("./controller/question");
+const tagController = require("./controller/tag");
+const answerController = require("./controller/answer");
+
+app.use("/question", questionController);
+app.use("/tag", tagController);
+app.use("/answer", answerController);
 
 let server = app.listen(port, () => {
     console.log(`Server starts at http://localhost:${port}`);
@@ -24,3 +73,5 @@ process.on("SIGINT", () => {
     console.log("Server closed. Database instance disconnected");
     process.exit(0);
 });
+
+module.exports = server
